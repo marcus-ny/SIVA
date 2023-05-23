8ba2af63c989f929d03bd4dd37649d2dbe7ff19c
marcus-ny <kyalsin321@gmail.com>
2023-05-23 22:06:59 +0800

    Added camera movement
    Added WASD movement as well as Edge scrolling movement for camera

---
+++ Project SIVA/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    private float cameraSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal") * cameraSpeed * Time.deltaTime;
+        float yAxisValue = Input.GetAxis("Vertical") * cameraSpeed * Time.deltaTime;
+
+        // transform.position = new Vector3(xAxisValue, yAxisValue, transform.position.z);
+
+        transform.Translate(new Vector3(xAxisValue, yAxisValue, 0));
+    }
+}
