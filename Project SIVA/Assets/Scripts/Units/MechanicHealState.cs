cad4685c96d749338dfc107d76f2e1c622ac73ed
marcus-ny <kyalsin321@gmail.com>
2023-06-06 17:55:05 +0800

    Halfway implementing foreground tile

---
+++ Project SIVA/Assets/Scripts/Units/MechanicHealState.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechanicHealState : MechanicBaseState
+{
+    public override void EnterState(Mechanic mechanic)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateState(Mechanic mechanic)
+    {
+        throw new System.NotImplementedException();
+    }
+}
