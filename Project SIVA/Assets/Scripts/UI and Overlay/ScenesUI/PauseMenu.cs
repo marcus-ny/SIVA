55319de92efb1d7a290cb8127f0faba510a232e7
IanFH <101397272+IanFH@users.noreply.github.com>
2023-06-09 00:55:08 +0800

    Added Some GUI Online, and fixed some pause menu

--- Project SIVA/Assets/Scripts/UI and Overlay/ScenesUI/PauseMenu.cs
+++ Project SIVA/Assets/Scripts/UI and Overlay/ScenesUI/PauseMenu.cs
@@ -19,7 +19,7 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    void Resume() {
+    public void Resume() {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
