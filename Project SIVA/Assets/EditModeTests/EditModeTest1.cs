b4b724be1ccc371deda7b1cdc5395ad327478b62
marcus-ny <kyalsin321@gmail.com>
2023-06-21 00:47:58 +0800

    Test testcases v1

---
+++ Project SIVA/Assets/EditModeTests/EditModeTest1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class EditModeTest1
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void EditModeTest1SimplePasses()
+    {
+        // Use the Assert class to test conditions
+        int x = 5;
+
+        Assert.AreEqual(x, 5);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator EditModeTest1WithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
