1465f610d92d1811e30b3f8e30746cc5ea694433
marcus-ny <kyalsin321@gmail.com>
2023-07-05 05:22:39 +0800

    Level Design
    - [ ] Made a level design base tilemap
    - [ ] Added some world entities such as lab testtubes

---
+++ Project_SIVA/Assets/Scripts/World Entities/LabTube.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabTube : WorldEntity
+{
+    private void Start()
+    {
+        activeTile.isBlocked = true;
+        foreach (OverlayTile tile in MapController.Instance.Get3x3Grid(activeTile))
+        {
+            tile.isBlocked = true;
+        }
+    }
+
+    public override void Highlight(bool trigger)
+    {
+        
+    }
+}
