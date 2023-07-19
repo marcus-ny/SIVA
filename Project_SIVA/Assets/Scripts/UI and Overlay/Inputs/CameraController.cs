34fb1e9d57f952d6f828cb0dc62a8e22caedb261
marcus-ny <kyalsin321@gmail.com>
2023-07-19 23:57:29 +0800

    Chapter 2 Level maps design + Dialogue templates added

--- Project_SIVA/Assets/Scripts/UI and Overlay/Inputs/CameraController.cs
+++ Project_SIVA/Assets/Scripts/UI and Overlay/Inputs/CameraController.cs
@@ -5,36 +5,40 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private float cameraSpeed;
+    private float initialX;
+    private float initialY;
 
     // Start is called before the first frame update
     void Start()
     {
         cameraSpeed = 5f;
+        initialX = transform.position.x;
+        initialY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKey(KeyCode.A) && transform.position.x > -5f)
+        if (Input.GetKey(KeyCode.A) && transform.position.x > initialX - 5)
         {
             float moveAmt = cameraSpeed * Time.deltaTime;
             transform.Translate(new Vector3(-moveAmt, 0, 0));
         }
 
-        if (Input.GetKey(KeyCode.D) && transform.position.x < 5f)
+        if (Input.GetKey(KeyCode.D) && transform.position.x < initialX + 5)
         {
             float moveAmt = cameraSpeed * Time.deltaTime;
             transform.Translate(new Vector3(moveAmt, 0, 0));
         }
 
-        if (Input.GetKey(KeyCode.W) && transform.position.y < 2.5f)
+        if (Input.GetKey(KeyCode.W) && transform.position.y < initialY + 2.5)
         {
             float moveAmt = cameraSpeed * Time.deltaTime;
             transform.Translate(new Vector3(0, moveAmt, 0));
         }
 
-        if (Input.GetKey(KeyCode.S) && transform.position.y > -2.5f)
+        if (Input.GetKey(KeyCode.S) && transform.position.y > initialY - 2.5)
         {
             float moveAmt = cameraSpeed * Time.deltaTime;
             transform.Translate(new Vector3(0, -moveAmt, 0));
